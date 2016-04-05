2ad4104f06015b42b8ea4fea689b3e61b6379769
Aquid Shahwar <aquid.shahwar@gmail.com>
2016-04-06 01:36:45 +0530

    user context middleware set in middleware/currentUser.js file.

---
+++ server/middleware/currentUser.js
@@ -0,0 +1,58 @@
+'use strict'
+var loopback = require('loopback');
+
+module.exports = function(){
+	return function currentUser(req,res,next){
+		// console.log("in the middleware"); // check if the middleware is working
+		var loopbackContext = loopback.getCurrentContext();
+		if(loopbackContext) {
+			// console.log(req);
+			if(!req.accessToken){
+				return next();
+			}
+			else {
+				// console.log(typeof req.accessToken.userId); check the id of the user is of type BSON object
+				loopbackContext.set('accessToken', req.accessToken);
+				// find the user from the token include its roles and organisation
+				req.app.models.orgUser.findById(req.accessToken.userId,{include:[
+					{
+						relation:'orgRoles',
+						scope : {
+							fields: ['name'] // only include the role name and id
+						}
+					}, 
+					{
+						relation: 'organisation',
+						scope : {
+							fields: ['name'] // only inclue the organisation name and ids
+						}
+					}
+				]})
+				.then(function(user){
+					if(!user){
+						return next(new Error('No user with this access token was found.'));
+					}
+					else{
+						/*
+						* Get the current user data with fetch its roles and organisation.
+						* set current user to context.
+						* set user roles to constext. 
+						* set user organisation to context.
+						*/
+						loopbackContext.set('currentUser', user);
+			          	loopbackContext.set('userRoles', user.orgRoles);
+			          	loopbackContext.set('organisation', user.ororganisationgRoles);
+			          	next();
+					}
+					next();
+				})
+				.catch(function(err){
+					next(err);
+				})
+			}
+		}
+		else{
+			next();
+		}
+	}
+}
