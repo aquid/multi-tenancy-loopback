97f4fabb1c8ef2dd4b5b1717b1add0b12e7a5231
Aquid Shahwar <aquid.shahwar@gmail.com>
2016-04-16 21:25:50 +0530

    dynamic role resolver added for members of the organisation with name as "memberForOrg" and ACL changes for the "organisation.json" files to support the dynamic roles

--- server/boot/org_role_resolver.js
+++ server/boot/org_role_resolver.js
@@ -3,6 +3,10 @@ var _ = require('underscore');
 
 module.exports = function(app){
 	var Role = app.models.Role;
+	/*
+	* Dynamic role resolver for admins (orgAdmin) of a particular organisation
+	* Checks for the user roles and only allows orgAdmin to execute
+	*/
 	Role.registerResolver('adminForOrg', function(role, context, cb) {
 		function reject(err) {
 			if(err) {
@@ -11,21 +15,77 @@ module.exports = function(app){
 			cb(null, false);
 		}
 		if(context.modelName !== 'organisation'){
+			// return error if target model is not organisation
 			return reject();
 		}
+		var currentUserId = context.accessToken.userId;
 		var currentOrg = context.modelId;
-
-		var loobackCotext = app.loopback.getCurrentContext();
-		var currentUserOrg = loobackCotext.get('organisation').id;
-		var currentUserRoles = loobackCotext.get('userRoles');
-		
-		var isAdmin = _.findWhere(currentUserRoles,{name: 'orgAdmin'});
-
-		if(!_.isEqual(currentOrg,currentUserOrg) || !isAdmin){
-			reject();
+		if(!currentUserId){
+			// Do not allow unauthenticated users to proceed
+			return reject();
 		}
-		else {
-			cb(null,true);
+		if(currentOrg){
+			var loobackCotext = app.loopback.getCurrentContext();
+			var currentUserOrg = loobackCotext.get('organisation');
+			var currentUserRoles = loobackCotext.get('userRoles');
+
+			var isAdmin = _.findWhere(currentUserRoles,{name: 'orgAdmin'});
+			// check if current organisation is equal to current users organisation 
+			// and current user has role as orgAdmin
+			if(!_.isEqual(currentOrg,currentUserOrg.id) || !isAdmin){
+				// return if false
+				return reject();
+			}
+			else {
+				return cb(null,true);
+			}
+		}
+	});
+
+	/*
+	* Dynamic role resolver for members(orgAdmin and storeAdmin) of organisation
+	* Checks for the user roles and allows both orgAdmin and storeAdmin to execute
+	*/
+	Role.registerResolver('memberForOrg', function(role,context,cb){
+		function reject(err) {  // return callback with error
+			if(err) {
+				return cb(err);
+			}
+			cb(null, false);
+		}
+		if(context.modelName !== 'organisation'){ 
+			// return error if target model is not organisation
+			return reject();
+		}
+		var currentUserId = context.accessToken.userId;
+		var currentOrg = context.modelId;
+		if(!currentUserId){
+			// Do not allow unauthenticated users to proceed
+			return reject();
+		}
+		if(currentOrg){
+			var loobackCotext = app.loopback.getCurrentContext();
+			var currentUserOrg = loobackCotext.get('organisation');
+			var currentUserRoles = loobackCotext.get('userRoles');
+
+			var isAdmin = _.findWhere(currentUserRoles,{name: 'orgAdmin'});
+			var isStoreAdmin = _.findWhere(currentUserRoles,{name: 'storeAdmin'});
+
+			// check if current organisation is equal to current users organisation 
+			if(!_.isEqual(currentOrg,currentUserOrg.id)){
+
+				reject();
+			}
+			else {
+				 // check if current user has either of orgAdmin or storeAdmin roles 
+				if(!isAdmin && !isStoreAdmin){
+					// return if user is neither orgAdmin nor storeAdmin
+					return reject();
+				}
+				else{
+					return cb(null,true);
+				}
+			}
 		}
 	});
 };
